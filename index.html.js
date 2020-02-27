3d26131f2fb28c8f830e1040c49c2f6d590d6b35
sam.chuang <sam.chuang@alliance.tw>
2020-02-27 17:45:31 +0800

    init: build index.html via js

---
+++ index.html.js
@@ -0,0 +1,28 @@
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import fs from "fs-extra"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+console.log("__filename", __filename, "__dirname", __dirname)
+
+//TODO: get version from package.json
+const document = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TensorFlow.js Sandbox</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <div id="app"></div>
+
+    <script type="module" src="dist/index.js"></script>
+  </body>
+</html>
+`
+
+//TODO: get dist folder from setting?
+fs.outputFileSync(`${__dirname}/dist/index.html`, document)
